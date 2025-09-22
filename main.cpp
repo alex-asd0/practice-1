b850606457a1edaab3d29c1c7e26b708bddfd302
alex <marenin.21alex@ya.ru>
2025-09-22 19:27:58 +0300

    fix issue: code style

--- main.cpp
+++ main.cpp
@@ -3,35 +3,44 @@
 #include <cstring>
 
 
-bool containing_letters(char* s1, size_t s1_len, char* s2, size_t s2_len) {
+const size_t MAX_WORDS_LENGTH = 32;
+
+bool ContainingLetters(char* s1, size_t s1_len, char* s2, size_t s2_len) {
 	for (size_t i = 0; i < s1_len; ++i) {
 		bool found = false;
 		for (size_t j = 0; j < s2_len; ++j) {
-			if (s1[i] == s2[j]) found = true;
+			if (s1[i] == s2[j]) {
+				found = true;
+			}
 		}
-		if (!found) return false;
+		if (!found) {
+			return false;
+		}
 	}
 
   	return true;
 }
 
+void ReadArgvString(char** argv, char* word) {
+	for (int j = 0; j < std::strlen(*argv); ++j) {
+		word[j] = (*argv)[j];
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc < 5) {
-		std::cout << "Error: not enough arguments" << std::endl;
+		std::cerr << "Error: not enough arguments" << '\n';
 		return 0;
 	}
-	char word[32];
-	char path[32];
+
+	char word[MAX_WORDS_LENGTH];
+	char path[MAX_WORDS_LENGTH];
 	for (int i = 0; i < argc; ++i) {
-		if (!std::strcmp(*argv, "--word")) {
-			for (int j = 0; j < std::strlen(*(argv + 1)); ++j) {
-				word[j] = (*(argv + 1))[j];
-			}
+		if (std::strcmp(*argv, "--word") == 0) {
+			ReadArgvString(argv + 1, word);
 		}
-		if (!std::strcmp(*argv, "--file")) {
-			for (int j = 0; j < std::strlen(*(argv + 1)); ++j) {
-				path[j] = (*(argv + 1))[j];
-			}
+		if (std::strcmp(*argv, "--file") == 0) {
+			ReadArgvString(argv + 1, path);
 		}
 		argv++;
 	}
@@ -39,12 +48,12 @@ int main(int argc, char** argv) {
 	std::ifstream stream;
 	stream.open(path);
 	if (!stream.is_open()) {
-		std::cout << "Erorr: can't open file";
+		std::cerr << "Erorr: can't open file" << '\n';
 		return 0;
 	}
 
-	for (char line[32]; stream >> line;){
-		if (containing_letters(word, std::strlen(word), line, std::strlen(line))) {
+	for (char line[MAX_WORDS_LENGTH]; stream >> line;){
+		if (ContainingLetters(word, std::strlen(word), line, std::strlen(line))) {
 			std::cout << line << '\n';
 		}
 	}
